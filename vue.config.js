853429757a2d7e8c707ed03767c42ce32ef57423
Lucy <ziyuzhao1025@gmail.com>
2024-12-31 16:02:10 -0500

    readme and final deployment

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? '/speed-typer/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/Speed-typer/' : '/',
 
   pluginOptions: {
     vuetify: {
